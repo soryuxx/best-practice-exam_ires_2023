e9aeab3c29b844346d28d670f079796a14709f25
Ana Potochina <potochina1@gmail.com>
2023-08-03 12:37:57 +0200

    part one working just fine

--- src/OldFashionPoundApp.java
+++ src/OldFashionPoundApp.java
@@ -4,13 +4,17 @@ public class OldFashionPoundApp {
   public static void main(String[] args){
     OldFashionPound a = new OldFashionPound(5, 17, 8);
     OldFashionPound b = new OldFashionPound(3, 5, 10);
+    OldFashionPound c = new OldFashionPound(8, 5, 10);
     Operations sum = new Operations();
     sum.sum(a, b);
     Operations sub = new Operations();
     sub.subtraction(a, b );
+    sub.subtraction(a, c);
     Operations mult = new Operations();
     mult.multiplication(a, 2);
     Operations div = new Operations();
+    OldFashionPound d = new OldFashionPound(18, 16, 1);
+    div.division(d, 15);
     div.division(a, 3);
 
 
